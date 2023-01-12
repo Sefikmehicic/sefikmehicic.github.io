de871ae132d9f2857c1ba6653b54ee61141ce2fb
Sefik Mehicic <sefik.mehicic@if.se>
2023-01-12 17:02:02 +0100

    Fixed modals and mock values for search history

--- src/app/search-history/search-history.component.ts
+++ src/app/search-history/search-history.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-search-history',
@@ -6,8 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-history.component.css']
 })
 export class SearchHistoryComponent implements OnInit {
+  modalRef?: BsModalRef;
+  backBtnText: string = 'Tillbaka'
 
-  constructor() { }
+  constructor(private modalService: BsModalService) { }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
+  }
 
   ngOnInit(): void {
   }
