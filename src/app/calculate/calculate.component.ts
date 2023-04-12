737c16ccda30b34ead37002a090c7c60470b054c
Sefik Mehicic <sefik.mehicic@if.se>
2023-04-12 16:33:58 +0200

    -

--- src/app/calculate/calculate.component.ts
+++ src/app/calculate/calculate.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-calculate',
@@ -12,30 +13,97 @@ export class CalculateComponent implements OnInit {
   customerId: string = "";
   showInfo: boolean = false;
 
-  constructor(private router: Router, private route: ActivatedRoute) {}
+  Rot: calc =
+    {
+      rows: [
+        { name: 'Bygg', hours: 0, materialCost: 0 },
+        { name: 'El', hours: 0, materialCost: 0 },
+        { name: 'Glas/Plåt', hours: 0, materialCost: 0 },
+        { name: 'Mark/Dränering', hours: 0, materialCost: 0 },
+        { name: 'Målning/Tapetsering', hours: 0, materialCost: 0 },
+        { name: 'Bygg', hours: 0, materialCost: 0 },
+        { name: 'VVS', hours: 0, materialCost: 0 }
+      ],
+      sumOfHours: 0,
+      sumOfMaterialCost: 0
+    };
 
-  onChange(value: any) {
+    Rut: calc =
+    {
+      rows: [
+        { name: 'Barnpassning', hours: 0, materialCost: 0 },
+        { name: 'Flyttjänster', hours: 0, materialCost: 0 },
+        { name: 'IT-tjänster', hours: 0, materialCost: 0 },
+        { name: 'Kläd- och textilvård', hours: 0, materialCost: 0 },
+        { name: 'Möblering', hours: 0, materialCost: 0 },
+        { name: 'Personlig omsorg', hours: 0, materialCost: 0 },
+        { name: 'Reparation av vitvaror', hours: 0, materialCost: 0 },
+        { name: 'Snöskottning', hours: 0, materialCost: 0 },
+        { name: 'Städning', hours: 0, materialCost: 0 },
+        { name: 'Tillsyn av bostad', hours: 0, materialCost: 0 },
+        { name: 'Trädgårdsarbete', hours: 0, materialCost: 0 }
+      ],
+      sumOfHours: 0,
+      sumOfMaterialCost: 0
+    };
+
+  constructor(private router: Router, private route: ActivatedRoute) { }
+
+  typeOfWork(value: any) {
     this.type = value.target.value;
-}
+  }
+
+  RotHourSum() {
+    this.Rot.sumOfHours = this.Rot.rows.map(i => i.hours).reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  }
+  RotMaterialCostSum() {
+    this.Rot.sumOfMaterialCost = this.Rot.rows.map(i => i.materialCost).reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  }
+
+  RutHourSum(){
+    this.Rut.sumOfHours = this.Rut.rows.map(i => i.hours).reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  }
+  RutMaterialCostSum(){
+    this.Rut.sumOfMaterialCost = this.Rut.rows.map(i => i.materialCost).reduce(function (a, b) {
+      return a + b;
+    }, 0);
+  }
 
   search(): void {
-    if(this.customerId){
+    if (this.customerId) {
       this.showInfo = true;
     }
-    else{
+    else {
       this.showInfo = false;
     }
   }
 
   ngOnInit(): void {
-    if(this.router.url.includes('id')){
+    if (this.router.url.includes('id')) {
       this.route.queryParams
-      .subscribe(params => {
-        this.customerId = params["id"];
-        this.showInfo = true;
-      }
-    );
+        .subscribe(params => {
+          this.customerId = params["id"];
+          this.showInfo = true;
+        }
+        );
     }
   }
+}
 
+export interface calc {
+  rows: rows[];
+  sumOfHours: number;
+  sumOfMaterialCost: number;
+}
+
+export interface rows {
+  name: string;
+  hours: number;
+  materialCost: number;
 }
