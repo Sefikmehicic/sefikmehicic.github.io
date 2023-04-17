9648e4cc3bebb402fba839c71243094501187a21
Sefik Mehicic <sefik.mehicic@if.se>
2023-04-17 23:25:31 +0200

    New design after "kvällsmys"

--- src/app/home/Navbar/navbar/navbar.component.ts
+++ src/app/home/Navbar/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -10,5 +10,18 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', ['$event'])
+
+onWindowScroll() {
+    let element = document.querySelector('.navbar') as HTMLElement;
+    if (window.pageYOffset > element.clientHeight) {
+      element.classList.add('navbar-inverse');
+      element.classList.add('shadow-sm');
+    } else {
+      element.classList.remove('navbar-inverse');
+      element.classList.remove('shadow-sm');
+    }
   }
 }
