abda8e6e948c7f6d8c40567e93cccfbebc2cb947
Sefik Mehicic <sefik.mehicic@if.se>
2023-04-25 16:06:21 +0200

    Minor changes.

--- src/app/home/Navbar/navbar/navbar.component.ts
+++ src/app/home/Navbar/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,29 +8,31 @@ import { Component, HostListener, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
   @HostListener('window:scroll', ['$event'])
 
-onWindowScroll() {
-    let navbar = document.querySelector('.navbar') as HTMLElement;
-    let logoColor = document.querySelector('#logo-color') as HTMLElement;
-    let logoWhite = document.querySelector('#logo-white') as HTMLElement;
+  onWindowScroll() {
+    if (!this.router.url.includes('About')) {
+      let navbar = document.querySelector('.navbar') as HTMLElement;
+      let logoColor = document.querySelector('#logo-color') as HTMLElement;
+      let logoWhite = document.querySelector('#logo-white') as HTMLElement;
 
-    if (window.pageYOffset > navbar.clientHeight) {
-      navbar.classList.add('navbar-inverse');
-      navbar.classList.add('shadow-sm');
-      logoWhite.classList.add('remove');
-      logoColor.classList.remove('remove');
-      
-    } else {
-      navbar.classList.remove('navbar-inverse');
-      navbar.classList.remove('shadow-sm');
-      logoWhite.classList.remove('remove');
-      logoColor.classList.add('remove');
+      if (window.pageYOffset > navbar.clientHeight) {
+        navbar.classList.add('navbar-inverse');
+        navbar.classList.add('shadow-sm');
+        logoWhite.classList.add('remove');
+        logoColor.classList.remove('remove');
+
+      } else {
+        navbar.classList.remove('navbar-inverse');
+        navbar.classList.remove('shadow-sm');
+        logoWhite.classList.remove('remove');
+        logoColor.classList.add('remove');
+      }
     }
   }
 }
